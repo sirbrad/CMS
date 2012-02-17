7c30c37cd741c1d77d11ed67b0a95a9f3cee9f88
Ashley Banks <ashleysmbanks89@gmail.com>
2012-02-17 12:36:49 +0000

    removed call to database in controller news

--- Controllers/News.php
+++ Controllers/News.php
@@ -1,7 +1,6 @@
 <?php
 /** Load these in order of importance **/
 $_router = Router::getInstance (); // Routes the uri
-$_db = DB_Class::getInstance (); // Use this if you want a database connection
 $_templater = Templater::getInstance (); // Load this if you want the templater
 
 // Include any files or helpers needed

7f20d949455968c629223efe771f4746372fc7be
Ashley Banks <ashleysmbanks89@gmail.com>
2012-02-17 12:13:31 +0000

    made some useful comments

--- Controllers/News.php
+++ Controllers/News.php
@@ -4,26 +4,40 @@ $_router = Router::getInstance (); // Routes the uri
 $_db = DB_Class::getInstance (); // Use this if you want a database connection
 $_templater = Templater::getInstance (); // Load this if you want the templater
 
+// Include any files or helpers needed
 include ( 'App/Helpers/Common.php' );
 
+// This can just go in the header really - but an example of how to load an include
 $tags['include_header'] = get_include ( 'header' );
+
+// Standard tags that should be set
 $tags['directory'] = DIRECTORY;
 $tags['site_name'] = SITE_NAME;
 
+// Set up the database columns at the start - used for saving and whatever
+$db_columns = array ( 'news_title',
+					  'news_content',
+					  'news_link',
+					  'news_twitter',
+					  'news_on' );
 
 $template = 'Templates/example_news';
 
 $method = $_router->get_controller_method ();
 
+// Pick up the controller method - this time edit.
 if ( $method == 'edit' )
 {
+	// Load the relevent model that you want
 	$mod = new News_model ();
 	
 	$value = $_router->get_method_value ();
 	
 	$_tags = $mod->get_values ( $value );
 	
+	// As the model gathered up tags we merge the two tag arrays to display
 	$tags = array_merge ( $_tags, $tags );
+	
 }
 
 $_templater->set_content ( $template, $tags );

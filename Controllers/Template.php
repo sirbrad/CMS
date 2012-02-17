c068b38846245bdfa742a77ba7e2a4488106a66c
Bradley Few <few.bradley@gmail.com>
2012-02-17 11:19:29 +0000

    Fixed up styling on edit template

--- Controllers/Template.php
+++ Controllers/Template.php
@@ -1,7 +1,7 @@
 <?php
 /** Load these in order of importance **/
 $_router = Router::getInstance (); // Routes the uri
-$_db = DB_Class::getInstance (); // Use this if you want a database connection
+//$_db = DB_Class::getInstance (); // Use this if you want a database connection
 $_templater = Templater::getInstance (); // Load this if you want the templater
 
 include ( 'App/Helpers/Common.php' );

9aee59a948ca7bba960df36f31c6155808c9770f
Bradley Few <few.bradley@gmail.com>
2012-02-21 17:53:59 +0000

    Modal successfully opening and closing

--- Assets/Scripts/Utils/modal.js
+++ Assets/Scripts/Utils/modal.js
@@ -13,45 +13,60 @@ define(['require', 'jquery', 'Utils/getDocHeight', 'Utils/getAppliedStyle'], fun
 	elem.id = 'modal';
 	elem.setAttribute('data-animation', 'fadein');
 	
-	function show() {
+	function showWindow() {
 		
 		
+		overlay.appendChild(elem);
 		frag.appendChild(overlay);
-		frag.appendChild(elem);
 		
 		// Append frag to our body
 		bod.appendChild(frag);
 	}
 	
-	function hide() {
+	function closeWindow() {
 		
-		elem.setAttribute('data-animation', 'fadeout');
-		bod.removeChild(overlay);
+		overlay.setAttribute('data-animation', 'fadeout');
 		
+		// Poll until our element has 0 opacity
+		var timer = setInterval(function(){
+			
+			// As soon as our element has zero opacity we remove
+			// it from the document and then clear interval
+			if (appliedStyle(overlay, 'opacity') <= 0) {
+				
+				document.body.removeChild(overlay);
+				
+				// Reset attribute
+				overlay.setAttribute('data-animation', 'null');
+				
+				clearInterval(timer);
+			}
+			
+		}, 10);
 		
-		// Need to remove element from document.
-		//appliedStyle(elem, 'opacity');
+		//console.log(appliedStyle(overlay, 'opacity'))
+		
 	}
 	
 	jQuery(overlay).bind('click', function(e){
 		
-		
-		hide();
+		closeWindow();
+			
 	});
 		
 	
 	return function(opts) {
 		
 		require(['tpl!Templates/modal.tpl'], function(template){
-				var modalOptions = template({ 
-					title: opts.title,
-					message:opts.message 
-				})
-				
-				elem.innerHTML = modalOptions;
-				
-				// Display our modal
-				show();
+			var modalOptions = template({ 
+				title: opts.title,
+				message:opts.message 
+			})
+			
+			elem.innerHTML = modalOptions;
+			
+			// Display our modal
+			showWindow();
 		});
 		
 		

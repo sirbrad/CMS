d650050cdc1fe8c36a15c1b422b6d02e058928f3
Bradley Few <few.bradley@gmail.com>
2012-02-23 13:00:49 +0000

    Prepare.js now showing input if checkbox:checked globally

--- Assets/Scripts/Utils/prepareElement.js
+++ Assets/Scripts/Utils/prepareElement.js
@@ -3,10 +3,27 @@ define(function(){
 	
 	var container = document.getElementsByTagName('form');
 	
-	
 	jQuery(container).bind('click', function(e){
 		var targ = e.target,
-		superParent;
+			input = this.getElementsByTagName('input'),
+			len = input.length,
+			superParent,
+			btn,
+			checkOptions;
+		
+		// Work out and store our submit btn
+		while (len--) {
+			if (input[len].type === 'submit') {
+				btn = input[len];
+			}
+			
+			if (input[len].checked) {
+				btn.removeAttribute('disabled');
+				break;
+			} else {
+				btn.setAttribute('disabled', 'disabled');
+			}
+		}
 		
 		// Check the user has interacted with our input
 		if (targ.tagName.toLowerCase() === 'input') {

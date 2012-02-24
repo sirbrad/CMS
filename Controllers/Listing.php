c93d04a76c0e5f4a2c7824cdffc5878f968a73d9
Ashley Banks <ashleysmbanks89@gmail.com>
2012-02-24 16:32:42 +0000

    got the conditional working in the listing was a damn captial letter

--- Controllers/Listing.php
+++ Controllers/Listing.php
@@ -12,6 +12,7 @@ $tags['alert'] = ' ';
 $tags['directory'] = DIRECTORY;
 $tags['site_name'] = SITE_NAME;
 $tags['script'] = 'main';
+$tags['add_another'] = FALSE;
 
 $template = 'listing';
 
@@ -20,6 +21,7 @@ $list_type = $_router->get_controller_method ();
 if ( $list_type == 'news' )
 {
 	$tags['edit_page'] = 'news/edit';
+	$tags['add_another'] = TRUE;
 	$table = 'news';
 }
 

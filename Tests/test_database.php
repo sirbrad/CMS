28f0380cd28137714e5c9a71afb011007f42459e
Ashley Banks <ashleysmbanks89@gmail.com>
2012-03-14 10:49:45 +0000

    finished testing the database class

--- Tests/test_database.php
+++ Tests/test_database.php
@@ -55,5 +55,36 @@ class Test_database extends UnitTestCase {
 		$this->assertTrue( $tables );	
 	}
 	
+	public function testInsert ()
+	{
+		$return = $this->db->insert ( 'framework_tests', array ( 'tests_title' => 'testing', 'tests_content' => 'testing content' ) );	
+		$this->assertTrue ( $return );
+		$this->assertNotNull( $return );
+	}
+	
+	public function testUpdate ()
+	{
+		$return = $this->db->where( 'tests_id', 1 )->update ( 'framework_tests', array ( 'tests_title' => 'testing', 'tests_content' => 'testing content' ) );
+		$this->assertTrue ( $return );
+	}
+	
+	public function testDescribe ()
+	{
+		$table = $this->db->describe_table ( 'framework_tests' );	
+		$this->assertTrue ( $table );
+		$this->assertNotNull ( $table );
+		
+		// Make a check that what is returned is an array
+		$array_check = TRUE;
+		
+		if ( !is_array ( $table ) )
+			$array_check = FALSE;
+		else
+			$array_check = TRUE;
+			
+		$this->assertTrue( $array_check );
+			
+	}
+	
 }
 ?>
